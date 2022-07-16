8db963578fbcd47bb3c09b92c5a14b523549d5bb
Wellington Brito <71724775+wellington86333@users.noreply.github.com>
2022-07-15 21:17:53 -0300

    Add files via upload

--- dividindo-arrays.js
+++ dividindo-arrays.js
@@ -1,7 +1,10 @@
-const nomes = ['João' , 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'Andre', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isaabela', 'Vinicius', 'Renan', 'Renata', 'Daisy', 'Camilo']
+const nomes = ['Joao','Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'Andre', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'vinivius', 'Renan', 'Renata', 'Daisy', 'Camilo']
 
-const sala1 = nomes.slice(0,nomes.length/2)
-const sala2 = nomes.slice(nomes.length/2)
-console.log(`Aunos da sala 1: ${sala1}`)
-console.log(`Aunos da sala 2: ${sala2}`)
-console.log(nomes.length)
+
+console.log("Tamanho da array", nomes.length);
+
+const sala1 = nomes.slice(0,nomes.length/2);
+const sala2 = nomes.slice(nomes.length/2);
+
+console.log(`Alunos da sala 1: ${sala1}`);
+console.log(`Alunos da sala 2: ${sala2}`)
